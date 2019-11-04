22f53e2b626d466dfeaea99aa093bb3962b69f5c
neha45556 <neha45556@gmail.com>
2019-11-04 13:08:26 -0800

    Update main.c

--- lab10-concurr/source/main.c
+++ lab10-concurr/source/main.c
@@ -12,12 +12,112 @@
 #include "simAVRHeader.h"
 #endif
 
+volatile unsigned char TimerFlag = 0;
+
+
+void TimerOn(){
+    TCCR1B = 0x0B;
+    OCR1A = 125;
+    TIMSK1 = 0x02;
+    TCNT1 = 0;
+    _avr_timer_cntcurr = _avr_timer_M;
+    SREG |= 0x80;  
+}
+    
+void TimerOff(){
+    TCCR1B = 0x00;
+}
+
+void TimerISR(){
+    TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect){
+	_avr_timer_cntcurr--;
+	if(_avr_timer_cntcurr == 0){
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+void TimerSet(unsigned long M){
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+} 
+unsigned char threeLeds = 0;
+unsigned char blinkingLED = 0;
+enum BL_States { BL_SMStart, BL_LedOff, BL_LedOn } BL_State;
+void TickFct_BlinkLed() {
+   switch(BL_State){
+       case BL_SMStart:
+           BL_State = BL_LedOff;
+           break;
+       case BL_LedOff:
+           BL_State = BL_LedOn;
+           break;
+       case BL_LedOn:
+           BL_State = BL_LedOff;
+           break;
+    }
+    switch(BL_State){
+        case BL_SMStart:
+            blinkingLED = 0x00;
+            break;
+        case BL_LedOff:
+            blinkingLED = 0x00;
+            break;
+        case BL_LedOn:
+            blinkingLED = 0x08;
+            break;
+    }
+}
+
+
+enum TL_States { start, TL_T0, TL_T1, TL_T2 } TL_State;
+
+void threeLed(){
+    switch(TL_State){
+        case start:
+            TL_State = TL_T0;
+            break;
+        case TL_T0: 
+            TL_State = TL_T1;
+            break;
+        case TL_T1:
+            TL_State = TL_T2;
+            break;
+        case TL_T2:
+            TL_State = TL_T0;
+            break;
+    }
+    switch(TL_State){
+        case start:
+            threeLeds = 0x00;
+            break;
+        case TL_T0:
+            threeLeds = 0x01;
+            break;
+        case TL_T1:
+            threeLeds = 0x02;
+            break;
+        case TL_T2:
+            threeLeds = 0x04;
+            break;
+    }
+}
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+    DDRB = 0xFF; PORTB = 0x00;
     /* Insert your solution below */
+    TimerSet(1000);
+    TimerOn();
+    BL_State  = BL_SMStart;
+    TL_State =  start;
     while (1) {
-
+        TickFct_BlinkLed();
+        threeLed();
+        while(!TimerFlag){}
+        TimerFLag = 0;
     }
     return 1;
 }
